7140b1af826ed73bc7002c8bf43b53fc9e3e62ff
Katia Bulekova <ktrn@bu.edu>
2021-08-19 16:57:37 -0400

    remove executabels from the standard packages and those scripts that are not used in the pipeline

--- bin/piRNA_target_pipeline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-INSTALLATION_PATH=/projectnb/lau-bumc/qichengm/software/mosquitoSmallRNA/bin
-DATABASE_PATH=/projectnb/lau-bumc/qichengm/software/mosquitoSmallRNA/database
-#input argument
-#$1 fastq file
-#$2 transcript file, e.g Angam_transcript_TE_virus.fa
-prefix=${1%.fa*}
-bash $INSTALLATION_PATH/piRNA_target_prediction.sh $2   $prefix
-bash $INSTALLATION_PATH/extract_water_alignment.sh   $prefix
