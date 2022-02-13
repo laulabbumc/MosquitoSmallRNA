7229acf56dff4403cea3c08aeefc548d6ab524db
laulabbumc <63164832+laulabbumc@users.noreply.github.com>
2022-02-12 23:39:15 -0500

    Delete container directory

--- container/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-IMAGE=/projectnb/lau-bumc/katia/MSRG/container/msrg.simg
-
-singularity exec -e -B /scratch -B /projectnb/lau-bumc/katia:/work $IMAGE /msrg/scripts/step1_gene-centric.sh $1
-
